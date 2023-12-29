65751e155f31e3a3abb56dda4d77c0c04f537374
Lakshman Karunasena <lakkaru@gmail.com>
2023-12-29 07:32:51 +0530

    feat: controller functions created

--- controller.js
+++ controller.js
@@ -1,15 +1,55 @@
+const User = require("./model");
 const users = [
   { id: 1, name: "LAKSHMAN" },
   { id: 2, name: "Prabash" },
 ];
 
-const getUsers = (cb) => {
-  cb(users);
+const getUsers = (req, res, next) => {
+  User.find()
+    .then((responce) => {
+      res.json({ responce });
+    })
+    .catch((error) => {
+      res.json({ error });
+    });
 };
-const getUserById = (id, cb) => {
-  const user = users.find((user) => user.id == id);
-  cb(user);
+const addUser = (req, res, next) => {
+  const user = new User({
+    id: req.body.id,
+    name: req.body.name,
+  });
+  User.save()
+    .then((responce) => {
+      res.json({ responce });
+    })
+    .catch((error) => {
+      res.json({ error });
+    });
+};
+
+const updateUser = (req, res, next) => {
+  const { id, name } = req.body;
+  User.updateOne({ id: id }, { $set: { name: name } })
+    .then((responce) => {
+      res.json({ responce });
+    })
+    .catch((error) => {
+      res.json({ error });
+    });
+};
+
+const deleteUser = (req, res, next) => {
+  const id = req.body.id;
+  User.deleteOne({ id: id })
+    .then((responce) => {
+      res.json({ responce });
+    })
+    .catch((error) => {
+      res.json({ error });
+    });
 };
 
 exports.getUsers = getUsers;
-exports.getUserById = getUserById;
+exports.addUser = addUser;
+exports.updateUser = updateUser;
+exports.deleteUser = deleteUser;
--- server.js
+++ server.js
@@ -1,4 +1,23 @@
-const app = require("./app");
+const { default: mongoose } = require("mongoose");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://lakkaru:Mo61633583@cluster0.5tnt4zp.mongodb.net/?retryWrites=true&w=majority";
+
+const connect = async () => {
+  try {
+    await mongoose.connect(uri);
+    console.log("Connected to Mongo db");
+  } catch (error) {
+    console.log("Mongo db Error: ", error);
+  }
+};
+connect();
 const server = app.listen(3001, "127.0.0.1", () => {
   console.log(`Node server is listning to ${server.address().port}`);
 });
